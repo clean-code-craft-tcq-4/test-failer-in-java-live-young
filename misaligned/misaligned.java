bc29838dc8524181d164c27336ba2ad87968a3ae
live-young <61771639+live-young@users.noreply.github.com>
2022-06-23 20:21:26 +0530

    Update misaligned.java

--- misaligned/misaligned.java
+++ misaligned/misaligned.java
@@ -2,8 +2,6 @@ public class misaligned {
 	
     static int printColorMap(String[] majorColors,String[] minorColors) { 
         int i = 0, j = 0;
-	    System.out.println(majorColors.length * minorColors.length + " pair color code");
-		System.out.println("Pair no|Major color|Minor color");
         for(i = 0; i < majorColors.length; i++) {
             for(j = 0; j <minorColors.length ; j++) {
                 System.out.println(formatColorMap(i,j, majorColors, minorColors));
@@ -13,15 +11,16 @@ public class misaligned {
     }
     
     static String formatColorMap (int i, int j, String[] majorColors,String[] minorColors) {	
-		return  (i * minorColors.length )+ j+1+"	"+ majorColors[i]+"	"+minorColors[j];	
+		return  (i * minorColors.length )+ j+1+"|"+ majorColors[i]+"|"+minorColors[j];	
     }
     
     public static void main(String[] args) { 
     	String[] majorColors = {"White", "Red", "Black", "Yellow", "Violet"};
         String[] minorColors = {"Blue", "Orange", "Green", "Brown", "Slate"};
-        
+        String checkResult = "13|Black|Green";
         int result = printColorMap(majorColors, minorColors);
         assert(result == 25);
+        assert(checkResult.equals(formatColorMap(2,3,majorColors,minorColors)));
         System.out.println("All is well (maybe!)");
     }
 }
