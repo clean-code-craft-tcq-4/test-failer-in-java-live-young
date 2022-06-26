3f23c470bf3853df367d384776633d6d41c7421d
live-young <61771639+live-young@users.noreply.github.com>
2022-06-26 22:58:41 +0530

    Delete AlertFailureTest.java

--- alerter/AlertFailureTest.java
+++
@@ -1,8 +0,0 @@
-public class AlertFailureTest {
-		public static void testAlertFailure(NetworkAlertStub networkStub) {
-			alerter.alertInCelcius(500.0f, networkStub);
-			assert (alerter.alertFailureCount == 1);
-			alerter.alertInCelcius(350.0f, networkStub);
-			assert (alerter.alertFailureCount == 2);
-		}
-	}
